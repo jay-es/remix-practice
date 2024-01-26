5007020fc3252e5036fe55b94232b4c22766a520
Jun Shindo <46585162+jay-es@users.noreply.github.com>
2024-01-27 00:25:28 +0900

    daisyui

---
+++ tailwind.config.ts
@@ -0,0 +1,9 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  content: ["./app/**/*.{ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("@tailwindcss/typography"), require("daisyui")],
+} satisfies Config;
